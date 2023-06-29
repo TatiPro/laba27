8c5f9ee8a1d0b496d9b0c39a262d2104ed94a62b
kretinina tatiana <kretinina1202@mail.ru>
2023-06-29 16:13:20 +0300

    Zadacha 2

--- main.js
+++ main.js
@@ -1,12 +1,54 @@
-var breadSlices =prompt("Введите количество ломтиков хлеба:");
-var cheeseSlices = prompt("Введите количество ломтиков сыра:");
-breadSlices = parseInt(breadSlices);
-cheeseSlices = parseInt(cheeseSlices);
+//Задача 1 
+const items = {
+    bread : 9,
+    cheese: 7,
+}
 
+const countSandwiches = (availableItems) => {
 
-function countMaxSandwiches(breadSlices,cheeseSlices){
-    let MaxSandwiches = Max.min(Math.floor(breadSlices/2),cheeseSlices);
-    return MaxSandwiches;  
+    const bread = availableItems.bread/2;
+    const cheese = availableItems.cheese ;
+    const maxSandwich =  Math.floor(Math.min(bread , cheese));
+    return maxSandwich
 
 }
-alert("countMaxSandwiches(bread:" + breadSlices + ",cheese:" + cheeseSlices + ") ->" + MaxSandwiches);
+
+console.log(countSandwiches(items))
+
+//Задача 2
+
+
+const number = 5;
+let brush = 0 ;
+function multiplication(number) {
+    columns = number
+    rows = number
+    if (brush === 0) {
+        let result = [];
+        for (let i = 0;  i <= rows; i++){
+            if (i === 0) {
+                result.push('#');
+            } else{
+                result.push(i);
+            }
+            
+        }
+        let myVar3 = result.join(' ')
+        console.log(myVar3)
+    }
+    for (let i = 0;  i <= columns; i++){
+        let result = [];
+        if (i > 0) {
+            result.push(i);
+            for (let j=1;  j <= rows; j++){
+                if (i * j !=0){
+                    result.push(i * j);
+                }
+            }
+            let myVar3 = result.join(' ')
+            console.log(myVar3)
+        }
+        
+    }
+}
+multiplication(number);
